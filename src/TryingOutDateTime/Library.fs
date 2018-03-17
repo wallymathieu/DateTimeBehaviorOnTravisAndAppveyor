dfff75a2d0689350a2eb842abbe51dace1253b2f
Oskar Gewalli <oskar@gewalli.se>
2018-03-17 11:59:57 +0100

    Adding tests of DateTime

--- src/TryingOutDateTime/Library.fs
+++ src/TryingOutDateTime/Library.fs
@@ -1,5 +1,13 @@
 namespace TryingOutDateTime
+open System
+open System.Globalization
+open FSharpPlus.Control
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name
+module internal Prelude =
+    let inline tupleToOption x = match x with true, value -> Some value | _ -> None
+open Prelude
+type FromFsharpPlus=
+
+
+    static member TryParseDateTime = fun x -> DateTime.TryParseExact       (x, [|"yyyy-MM-ddTHH:mm:ss.fffZ"; "yyyy-MM-ddTHH:mm:ssZ"|], null, DateTimeStyles.RoundtripKind) |> tupleToOption : option<DateTime>
+    static member TryParseDateTimeOffset = fun x -> DateTimeOffset.TryParseExact (x, [|"yyyy-MM-ddTHH:mm:ss.fffK"; "yyyy-MM-ddTHH:mm:ssK"|], null, DateTimeStyles.RoundtripKind) |> tupleToOption : option<DateTimeOffset>
--- tests/TryingOutDateTime.Tests/Tests.fs
+++ tests/TryingOutDateTime.Tests/Tests.fs
@@ -1,13 +1,31 @@
 module Tests
 
-
+open System
 open Expecto
 open TryingOutDateTime
-
+open FSharpPlus
+open System.Globalization
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "Say hello all" <| fun _ ->
-      let subject = Say.hello "all"
-      Expect.equal subject "Hello all" "You didn't say hello"
+  testList "TryParse" [
+    testCase "DateTime kind" <| fun _ ->
+      let dt =DateTime(2011,3,4,12,42,19)
+      let expected = DateTimeKind.Unspecified
+      Expect.equal expected dt.Kind "Manually initialized datetime should equal"
+    testCase "DateTime.FSharpPlus" <| fun _ ->
+      let v1 : DateTime       = parse "2011-03-04T15:42:19+03:00"
+      let expected =DateTime(2011,3,4,12,42,19)
+      Expect.equal expected v1 "Manually initialized datetime should equal"
+    testCase "DateTimeOffset.FSharpPlus" <| fun _ ->
+      let v2 : DateTimeOffset = parse "2011-03-04T15:42:19+03:00"
+      let expected = DateTimeOffset(2011,3,4,15,42,19, TimeSpan.FromHours 3.)
+      Expect.equal expected v2 "Manually initialized offset should equal"
+    testCase "DateTime.FSharpPlus.Copy" <| fun _ ->
+      let v1 = FromFsharpPlus.TryParseDateTime "2011-03-04T15:42:19+03:00"
+      let expected =Some (DateTime(2011,3,4,12,42,19))
+      Expect.equal expected v1 "Manually initialized datetime should equal"
+    testCase "DateTimeOffset.FSharpPlus.Copy" <| fun _ ->
+      let v2 = FromFsharpPlus.TryParseDateTimeOffset "2011-03-04T15:42:19+03:00"
+      let expected = Some (DateTimeOffset(2011,3,4,15,42,19, TimeSpan.FromHours 3.))
+      Expect.equal expected v2 "Manually initialized offset should equal"
   ]
